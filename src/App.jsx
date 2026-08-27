3d64726d8002fc3cc56f093fc23562d75adc663c
Agien99 <hypertyzer@gmail.com>
2026-08-27 16:07:24 +0800

    Added Skills section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import About from './components/About/About';
 import Experience from './components/Experience/Experience';
-import Experience from './components/Skills/Skills';
+import Skills from './components/Skills/Skills';
 
 function App() {
   return (
